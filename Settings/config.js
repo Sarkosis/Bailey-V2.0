972e69f5c9effb921859d4da18f839bb6174866e
Mr.Gamer <nicklaus.vega21@gmail.com>
2023-02-08 14:45:30 -0600

    config update

--- Settings/config.js
+++ Settings/config.js
@@ -2,25 +2,25 @@ const config = {
   bot: {
     clientID: "1047288679179231322", // Bot ID.
     token:
-      "MTA0NzI4ODY3OTE3OTIzMTMyMg.GxljKu.GwkCdML0lH6z9pzvFjFKqIMxXgMonpTn6yT3xI", // Bot Token,
+      "MTA0ODA3Mjg0ODAxMjQ3NjQ1Ng.GASxK1.6RQqW5_rWMAqftIkYr2LNpu6ChkAnkCnCB1Tb8", // Bot Token,
     debugMode: true, // Enable/Disable debug mode
   },
   theme: {
     color: "#152238", // Color theme usng HEX code.
   },
   stats: {
-    presence: "over the server", // custom presnce.
-    activity: "Watching", // activity type; Competing in, Playing, Watching, Listening to, Streaming.
+    presence: "bed", // custom presnce.
+    activity: "Competing in", // activity type; Competing in, Playing, Watching, Listening to, Streaming.
     status: "dnd", // idle, dnd, online, invisible.
   },
   tickets: {
     ticketlimit: 1,
   },
   database: {
-    host: "localhost", // Database host
-    user: "root", // Database user
-    password: "", // Database password
-    name: "negnbot", // The mysql ban database
+    host: "dev.c9ajfzgmntyu.us-east-1.rds.amazonaws.com", // Database host
+    user: "admin", // Database user
+    password: "EVjAjVuaHVmXvbQlxxUA", // Database password
+    name: "project-bailey", // The mysql ban database
   },
   modules: {
     tickets: true, // Should the tickets module be enabled?
